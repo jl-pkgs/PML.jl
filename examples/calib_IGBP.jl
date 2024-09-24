16367f569c5d62af51cde98f1950b3bdc22579a1
Dongdong Kong <kongdd@users.noreply.github.com>
2024-09-24 20:44:44 +0800

    v0.1.0 released

--- examples/calib_IGBP.jl
+++ examples/calib_IGBP.jl
@@ -1,5 +1,6 @@
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
 # # Calibrate PMLV2 model using FLUXNET data
+# > US Twt (CRO, 2009)
 using PML, Ipaper
 
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
@@ -35,18 +36,22 @@ function plot_var(var; label=string(var), title=string(var),
   data=df_out, scale=1, kw...)
   plot(t[inds], data[inds, var] * scale; label, title, kw...)
 end
-function plot_var!(p, var; label=string(var), kw...)
-  plot!(p, t[inds], df_out[inds, var]; label, kw...)
+function plot_var!(p, var; label=string(var),
+  data=df_out, kw...)
+  plot!(p, t[inds], data[inds, var]; label, kw...)
 end
 
-p1 = plot(title="ET components (mm/d)")
-plot_var!(p1, :Ec)
-plot_var!(p1, :Es)
-plot_var!(p1, :Ei)
+p_et = plot(title="ET components (mm/d)")
+plot_var!(p_et, :Ec)
+plot_var!(p_et, :Es)
+plot_var!(p_et, :Ei)
+plot_var!(p_et, :ETobs; data=df, label="ET_obs", color=:black)
+
+p_gpp = plot_var(:GPP; title="GPP (gC m-2 d-1)", label="GPP")
+plot_var!(p_gpp, :GPPobs; data=df, label="GPP_obs", color=:black)
 
 plot(
-  p1,
-  plot_var(:GPP; title="GPP (gC m-2 d-1)", label="GPP"),
+  p_et, p_gpp,
   plot_var(:Eeq; title="Eeq (mm/d)", label="Eeq"),
   plot_var(:Gc_w; title="Conductance (m s-1)", label="Gc"),
   plot_var(:fval_soil; title="β_soil", label="β_soil"),
@@ -54,5 +59,5 @@ plot(
   plot_var(:Pi; title="P - Ei (mm/d)"),
   plot_var(:Es_eq; title="Es_eq (mm/d)"),
   size=(800, 700),
-  layout=(4, 2)
+  layout=(4, 2), 
 )
