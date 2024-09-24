ce52d6262a0dcd574b5dbc655cf954859a853a0a
Documenter.jl <documenter@juliadocs.github.io>
2024-09-24 11:38:53 +0000

    build based on 66ad888

--- dev/generated/calib_IGBP.jl
+++ dev/generated/calib_IGBP.jl
@@ -1,9 +1,22 @@
-using PML, Ipaper
-
 using Plots
+gr(framestyle=:box)
 x = range(0, stop=6π, length=1000)
 y1 = sin.(x)
 y2 = cos.(x)
 plot(x, [y1, y2])
+
+using PML, Ipaper
+
+df_out, df, par = deserialize(file_FLUXNET_CRO)
+df.GPP_obs = df.GPPobs
+df.ET_obs = df.ETobs
+
+theta, goal, flag = model_calib(df, par0)
+df_out = PMLV2_sites(df; par=theta2par(theta))
+
+gof = [
+  (; var="ET", GOF(df.ET_obs, df_out.ET)...),
+  (; var="GPP", GOF(df.GPP_obs, df_out.GPP)...)] |> DataFrame
+DataFrame(gof)
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
